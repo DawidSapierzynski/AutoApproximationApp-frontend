61aa67bb0013b488bbb0cf58a78a415c6065cc8e
Dawid Sapierzyński <dawid.sapierzynski@student.wat.edu.pl>
2020-01-09 13:53:01 +0100

    Dodanie wyświetlania id serii dla właściwości aproksymacji

--- src/app/dto/ApproximationPropertiesDTO.ts
+++ src/app/dto/ApproximationPropertiesDTO.ts
@@ -3,6 +3,7 @@ import {DataSeriesFileDTO} from './DataSeriesFileDTO';
 export class ApproximationPropertiesDTO {
   public id: number;
   public userId: number;
+  public dataSeriesFileId: number;
   public dataSeriesFileDTO: DataSeriesFileDTO;
   public precision: number;
   public dateCreate: Date;
